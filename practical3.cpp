5485c293182448577ebd5a5a059720ad6c7660e0
Ect6969 <160695357+Ect6969@users.noreply.github.com>
2024-03-01 19:49:32 +0530

    third-file

---
+++ practical3.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main(){
+  int n1,n2,sum;
+  cout<<"Enter number for n1 = ";
+  cin>>n1;
+  cout<<"Enter number for n2 = ";
+  cin>>n2;
+  sum=n1+n2;
+  cout<<"Sum is equal to "<</t sum<<endl;
+  return 0;
+}
