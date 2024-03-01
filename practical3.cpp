60ec099cb10a27158bf4082b619f8a5625fb17da
Ect6969 <160695357+Ect6969@users.noreply.github.com>
2024-03-01 20:09:25 +0530

    Add files via upload

--- practical3.cpp
+++ practical3.cpp
@@ -1,12 +1,10 @@
 #include<iostream>
 using namespace std;
 int main(){
-  int n1,n2,sum;
-  cout<<"Enter number for n1 = ";
-  cin>>n1;
-  cout<<"Enter number for n2 = ";
-  cin>>n2;
-  sum=n1+n2;
-  cout<<"Sum is equal to "<< sum<<endl;
-  return 0;
+    int age;
+    cout<<"Enter your age = ";
+    cin>>age;
+
+    cout<<"your  age = "<<age<<endl;
+    return 0;
 }
